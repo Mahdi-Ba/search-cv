fd1ee8f598a31508557b6766b9c00e92bb1f53f8
Mahdi Bahari <baharimahdi93@gmail.com>
2020-05-04 15:48:46 +0430

    test

--- companies/admin.py
+++ companies/admin.py
@@ -11,14 +11,14 @@ class StatusAdmin(admin.ModelAdmin):
 
 
 
-@admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ['title', 'en_title', 'owner', 'size', 'sort', 'status', 'user', 'updated_at']
-    search_fields = ['title', 'en_title']
-    readonly_fields = ['user','owner']
-    list_filter = ['status']
-    autocomplete_fields = ['working_area']
-
-    def save_model(self, request, obj, form, change):
-        obj.user = request.user
-        super().save_model(request, obj, form, change)
+# @admin.register(Company)
+# class CompanyAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'en_title', 'owner', 'size', 'sort', 'status', 'user', 'updated_at']
+#     search_fields = ['title', 'en_title']
+#     readonly_fields = ['user','owner']
+#     list_filter = ['status']
+#     autocomplete_fields = ['working_area']
+#
+#     def save_model(self, request, obj, form, change):
+#         obj.user = request.user
+#         super().save_model(request, obj, form, change)

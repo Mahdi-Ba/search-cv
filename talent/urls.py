8abc1c3b571a6524132b527ee4f0d5f5ea2d4b0b
Mahdi Bahari <baharimahdi93@gmail.com>
2020-05-06 21:56:14 +0430

    swagger

--- talent/urls.py
+++ talent/urls.py
@@ -21,8 +21,40 @@ from django.conf.urls import url, include
 
 from rest_framework import permissions
 
+#
+# from django.conf.urls import url
+# from rest_framework_swagger.views import get_swagger_view
+#
+# schema_view = get_swagger_view(title='Pastebin API')
+
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
 
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),git
+
+
+    # url(r'^$', schema_view),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/v1/users/', include('users.urls')),
